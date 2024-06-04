fb77d0b764c0905d30f6af3a3b2e059a6a26526b
jannik-eggert <jannik.eggert@allianz.de>
2024-06-04 17:46:52 +0200

    added subscriptability

--- py_helpers/point.py
+++ py_helpers/point.py
@@ -20,7 +20,20 @@ class Point:
         else:
             return Point(x=self.x * other, y=self.y * other)
 
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        elif item == 1:
+            return self.y
+        else:
+            raise ValueError("A Point only has two elements")
+
     def value(
         self,
     ) -> int:
         return sqrt(self.x * self.x + self.y * self.y)
+
+
+if __name__ == "__main__":
+    print(Point(x=1.2, y=2.0)[0])
+    print(Point(x=1.2, y=2.0)[1])
