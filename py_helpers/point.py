322e58fc78be31b136685dab2146470070e6cc17
jannik-eggert <jannik.eggert@allianz.de>
2024-06-04 17:31:01 +0200

    enabled pointwise right-wise mult

--- py_helpers/point.py
+++ py_helpers/point.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from math import sqrt
 
+
 @dataclass
 class Point:
     x: float
@@ -10,11 +11,16 @@ class Point:
     def __add__(self, other: Point) -> Point:
         return Point(x=self.x + other.x, y=self.y + other.y)
 
+    def __rmul__(self, other):
+        return self.__mul__(other=other)
+
     def __mul__(self, other):
         if isinstance(other, Point):
             return self.x * other.x + self.y * other.y
         else:
-            return Point(x= self.x*other, y=self.y*other)
-        
-    def value(self, ) -> int: 
-        return sqrt(self.x*self.x + self.y*self.y)
+            return Point(x=self.x * other, y=self.y * other)
+
+    def value(
+        self,
+    ) -> int:
+        return sqrt(self.x * self.x + self.y * self.y)
